0ffa7eec3ab6b05f924c6e992876c8c97fd3dabc
zlata28 <larik288@yandex.ru>
2023-08-15 20:54:14 +0300

    rab

--- alert.js
+++ alert.js
@@ -1,1 +1,43 @@
-alert("Я JavaScript!")
+console.log("Я JavaScript!");
+
+let web = 3
+var web2 = 3
+const web3 = 4
+
+console.log(web,web2,web3)
+
+web = 5
+web2 =6
+
+console.log(web,web2,web3)
+
+
+
+let admin = ""
+let name = "Djon"
+
+admin = name
+
+alert(admin)
+
+
+let a = S;
+let b;
+
+b = a
+
+let age =prompt("Какого года ты рождения?")
+
+let result = 2023 - age
+
+let name = "Ваш возраст:"
+
+let  id = 5
+
+alert(`${name}`+result)
+
+let url = `locahost:3000/post/${id}`
+
+console.log(url)
+
+
