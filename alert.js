4d63877237c01f5327375cfc6aaa708a3c6a1109
zlata28 <larik288@yandex.ru>
2023-08-24 21:15:52 +0300

    1

--- alert.js
+++ alert.js
@@ -1,43 +1,46 @@
-console.log("Я JavaScript!");
+//let web = 3
+//var web2 = 3
+//const web3 = 4
 
-let web = 3
-var web2 = 3
-const web3 = 4
+//console.log(web,web2,web3)
 
-console.log(web,web2,web3)
+//web = 5
+//web2 =6
 
-web = 5
-web2 =6
-
-console.log(web,web2,web3)
+//console.log(web,web2,web3)
 
 
 
-let admin = ""
-let name = "Djon"
+//let admin = ""
+//let name = "Djon"
 
-admin = name
+//admin = name
 
-alert(admin)
+//alert(admin)
 
 
-let a = S;
-let b;
+//let a = S;
+//let b;
 
-b = a
+//b = a
 
-let age =prompt("Какого года ты рождения?")
+//let age =prompt("Какого года ты рождения?")
 
-let result = 2023 - age
+//let result = 2023 - age
 
-let name = "Ваш возраст:"
+//let name = "Ваш возраст:"
 
-let  id = 5
+//let  id = 5
 
-alert(`${name}`+result)
+//alert(`${name}`+result)
 
-let url = `locahost:3000/post/${id}`
+//let url = `locahost:3000/post/${id}`
 
-console.log(url)
+//console.log(url)
 
+
+let num; 
+let num = prompt (`Число больше 100`);
+do {alert(num);num++}
+while (num<=100);  
 
