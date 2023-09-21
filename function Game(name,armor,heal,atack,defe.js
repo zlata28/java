c590efcf6df691f84cd6600bb37475999d3fe83e
zlata28 <larik288@yandex.ru>
2023-09-21 21:18:30 +0300

    13

--- function Game(name,armor,heal,atack,defe.js
+++ function Game(name,armor,heal,atack,defe.js
@@ -11,14 +11,7 @@ const var1 = new Game("Doni","sword",100,10,10);
 const var2 = new Game("Oleg","sword",100,15,5);
 
 function duel(var1,var2){
-    
-    while(var1.heal > 0 && var2.heal > 0){
-        var1.heal -=(var2.atack - var1.defender)
-        var2.heal -=(var1.atack - var2.defender)
-        console.log('жизни ${var1.name}: ${var1.heal}')
-        console.log('жизни ${var2.name}: ${var2.heal}')
-    }
-    
+
 }
 
 duel(var1,var2)
