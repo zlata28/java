af9fbf0d3f39cb2db14dfe1fef806adc87fac2c3
zlata28 <larik288@yandex.ru>
2023-09-21 21:18:39 +0300

    111

--- Untitled-rab.js
+++ Untitled-rab.js
@@ -15,6 +15,13 @@ function duel(var1,var2){
     while(var1.heal > 0 && var2.heal > 0){
         var1.heal -=(var2.atack - var1.defender)
     }
+}    
+while(var1.heal > 0 && var2.heal > 0){
+    var1.heal -=(var2.atack - var1.defender)
+    var2.heal -=(var1.atack - var2.defender)
+    console.log('жизни ${var1.name}: ${var1.heal}')
+    console.log('жизни ${var2.name}: ${var2.heal}')
 }
+
 
 duel(var1,var2)
